5c872ab8a383654372fa8d90553194997c978d6c
promubsorn <ext.promubsorn.boo@truedigital.com>
2023-09-12 15:22:06 +0700

    updated lab exercise 1

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -1,4 +1,5 @@
-class Rational { 
+
+class Rational {
     long numerator,denominator; 
 
     class Illegal extends Exception { 
@@ -6,14 +7,18 @@ class Rational {
         Illegal (String reason) { 
             this.reason = reason; 
         } 
-    } 
+    }
 
     Rational() {
         // to be completed
+        System.out.println("Constructor Called");
     }
 
-    Rational(long numerator, long denominator) throws Illegal { 
+    Rational(long numerator, long denominator) throws Illegal {
         // to be completed
+        this.numerator = numerator;
+        this.denominator = denominator;
+        simplestForm();
     } 
 
     // find the reduce form 
@@ -46,6 +51,9 @@ class Rational {
      */
     public void subtract(Rational x) {
         // to be completed
+        numerator = (numerator * x.denominator) - (x.numerator * denominator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -54,6 +62,9 @@ class Rational {
      */
     public void multiply(Rational x) { 
         // to be completed
+        numerator = (numerator * x.numerator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -62,6 +73,9 @@ class Rational {
      */
     public void divide(Rational x) {
         // to be completed
+        numerator = (numerator * x.denominator);
+        denominator = (denominator * x.numerator);
+        simplestForm();
     }
 
     /***
@@ -69,9 +83,13 @@ class Rational {
      * @param x the rational number to be compared to the current rational number
      * @return true if the given rational number equals to the current, false otherwise
      */
+
     public boolean equals(Object x) {
         // to be completed
-        return true; // TODO: This needs to be modified.
+        if ( x instanceof Rational) {
+            Rational y = (Rational) x;
+            return (numerator == y.numerator) && (denominator == y.denominator); // TODO: This needs to be modified
+        } else return false;
     }
 
     /***
@@ -82,7 +100,13 @@ class Rational {
      */
     public long compareTo(Object x) {
         // to be completed
-        return -1; // TODO: this needs to be modified.
+        Rational y = (Rational) x;
+        this.subtract(y);
+        if ( numerator < 0 || denominator < 0){
+            return -1;
+        } else if (( numerator > 0 && denominator > 0)){
+            return 1;
+        } else return 0;
     }
 
     /***
@@ -91,7 +115,7 @@ class Rational {
      */
     public String toString() { 
         // to be completed
-        return ""; // TODO: This needs to be modified.
+        return this.numerator + "/" + this.denominator; // TODO: This needs to be modified.
     }
 
     public static void main(String[] args) {
--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -3,6 +3,17 @@ import org.junit.Test;
 
 public class RationalTest {
     @Test
+    public void testConstructor() throws Rational.Illegal {
+        //TO DO
+        Rational w = new Rational();
+        Object y = new Rational();
+        Assert.assertEquals(y , w);
+
+        Rational x = new Rational(1, 2);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(2, x.denominator);
+    }
+    @Test
     public void testAdd() {
         Rational x = new Rational();
         x.numerator = 1;
@@ -12,5 +23,69 @@ public class RationalTest {
         y.denominator = 4;
         x.add(y);
         Assert.assertEquals(3, x.numerator);
+    }
+    @Test
+    public void testSubtract() {
+        Rational x = new Rational();
+        x.numerator = 1;
+        x.denominator = 2;
+        Rational y = new Rational();
+        y.numerator = 1;
+        y.denominator = 4;
+        x.subtract(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(4, x.denominator);
+    }
+    @Test
+    public void testMultiply() {
+        Rational x = new Rational();
+        x.numerator = 1;
+        x.denominator = 2;
+        Rational y = new Rational();
+        y.numerator = 1;
+        y.denominator = 4;
+        x.multiply(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(8, x.denominator);
+    }
+
+    @Test
+    public void testDivide() throws Rational.Illegal {
+        Rational x = new Rational(1, 2);
+        Rational y = new Rational(1, 4);
+        x.divide(y);
+        Assert.assertEquals(2, x.numerator);
+        Assert.assertEquals(1, x.denominator);
+    }
+
+    @Test
+    public void testEqual() throws Rational.Illegal {
+        Rational x = new Rational(2, 4);
+        Rational y = new Rational(1, 2);
+        Rational z = new Rational(2, 3);
+        Assert.assertTrue(x.equals(y));
+        Assert.assertTrue(y.equals(x));
+        Assert.assertFalse(x.equals(z));
+    }
+
+    @Test
+    public void testCompareTo() throws Rational.Illegal {
+        Rational x = new Rational(1, 2);
+        Rational y = new Rational(2, 4);
+        Assert.assertEquals(0, x.compareTo(y));
+
+        Rational w = new Rational(1, 3);
+        Rational v = new Rational(2, 3);
+        Assert.assertEquals(1, v.compareTo(w));
+
+        Rational a = new Rational(1, 3);
+        Rational b = new Rational(2, 3);
+        Assert.assertEquals(-1, a.compareTo(b));
+    }
+
+    @Test
+    public void testToString() throws Rational.Illegal {
+        Rational x = new Rational(1, 2);
+        Assert.assertEquals("1/2", x.toString());
     }
 }
